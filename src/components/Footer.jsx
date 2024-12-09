141a9631104bd9dabdd6bb2e1e7f5f7d50a0cb87
Alin Olteanu <olteanualin1509@gmail.com>
2024-12-09 18:30:42 +0200

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='text-center border-t'>
+      <p className='py-3'><span className='font-bold'>CONA IT Solutions</span> © - 2024</p>
+    </div>
+  )
+}
