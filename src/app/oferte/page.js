ab20a2910c7470a3a22fd762d1f0ae9bcd278b10
Alin Olteanu <olteanualin1509@gmail.com>
2024-12-16 20:52:31 +0200

    Add cards for offers

--- src/app/oferte/page.js
+++ src/app/oferte/page.js
@@ -4,26 +4,25 @@ import { useContext } from 'react'
 import { ClientiContext } from '@/lib/context/clienti'
 
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import {
-  Ellipsis,
-  BookOpenText,
-  SquarePen,
+  User,
+  Calendar,
+  Cross,
   Trash
 } from 'lucide-react';
 import { OferteContext } from '@/lib/context/oferte'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export default function Oferte() {
  
@@ -33,7 +32,7 @@ export default function Oferte() {
   const [oferteObj, oferteInputs] = useContext(OferteContext);
  
   const {
-    oferta,
+    oferta: oferte,
     handleAdaugareOferta,
     removeOferta,
   } = oferteObj
@@ -47,7 +46,7 @@ export default function Oferte() {
 
   return (
     <>
-     <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center ">
         <div className="border-2 border-black p-4 w-[450px]  mb-8 mt-4 p-4 shadow-2xl ">
 
           <p className="text-center text-4xl mt-2">Adăugare Ofertă</p>
@@ -62,7 +61,6 @@ export default function Oferte() {
               onChange={handleNumeOferta}
               value={numeOferta}
             />
-           
 
             <p>Alege clientul:</p>
             <select value={numeClient} onChange={handleNumeClient} className='mt-2 border-b focus:outline-none  appearance-none bg-transparent '>
@@ -79,8 +77,6 @@ export default function Oferte() {
                 <option>Nu exista clienti in baza de date</option>
               )}
             </select>
-
-           
           </div>
 
           <button
@@ -92,60 +88,50 @@ export default function Oferte() {
         </div>
       </div>
 
-      <div>
-
-<ul>
-  {oferta.length === 0 ? (
-    <p className="text-center text-lg">Tabelul nu are informații introduse.</p>
-  ) : (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[150px] text-center">Nume Oferta</TableHead>
-          <TableHead className="text-center">Nume client</TableHead>
-          <TableHead className="text-right w-[70px]">Acțiuni</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {oferta.map((oferta, index) => (
-          <TableRow key={index}>
-            <TableCell className="text-center">{oferta.nume}</TableCell>
-            <TableCell className="text-center">{oferta.numeClient}</TableCell>
-            <TableCell className="text-center">
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <Ellipsis className="hover:bg-border rounded-full" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem>
-                    <BookOpenText />
-                    <span>Detalii</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <SquarePen />
-                    <span>Editare</span>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Trash />
-                    <span
-                      className="hover:cursor-pointer"
-                      onClick={() => removeOferta(index)}
-                    >
-                      Șterge
-                    </span>
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  )}
-
-</ul>
-
-</div>
+      <div className='grid grid-cols-4 gap-2'>
+        {oferte.map((oferta, index)=>
+          <Card key={index} className='relative'>
+            <CardHeader>
+              <CardTitle className="text-xl">{oferta.nume}</CardTitle>
+              <CardDescription className="flex items-center -ms-1"><User className='scale-75'/> {oferta.numeClient}</CardDescription>
+              <CardDescription className="flex items-center -ms-1"><Calendar className='scale-75'/> Ultima modificare: {new Date().toLocaleDateString()}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>Produse: <span className='font-bold'>5</span></p>
+              <p>Pret total: <span className='font-bold text-lg'>1140.50 LEI</span></p>
+            </CardContent>
+            <CardFooter className="grid grid-cols-2 gap-2">
+              <Button variant="secondary">Edit</Button>
+              <Button>Export</Button>
+            </CardFooter>
+            
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button 
+                  size="icon" 
+                  variant="ghost" 
+                  className="absolute top-0 right-0"
+                >
+                  <Trash className='stroke-gray-400'/>
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Esti sigur ca vrei sa stergi oferta?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Aceasta actiune nu este reversibila iar oferta va fi stearsa permanent
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Anulare</AlertDialogCancel>
+                  <AlertDialogAction onClick={()=> removeOferta(index)}>Sterge Oferta</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+            
+          </Card>
+        )}
+      </div>
     </>
   )
 }
